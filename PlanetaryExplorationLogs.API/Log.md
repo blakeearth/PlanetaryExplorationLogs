a8875eedb62219f95d11f34313bb38585e0d2c6e
Isaac Blake <blakeearth@users.noreply.github.com>
2024-11-09 11:12:08 -0600

    Add general client and server structure

---
+++ PlanetaryExplorationLogs.API/Log.md
@@ -0,0 +1,5 @@
+# Hi
+
+## 11/8/2024
+- I spent some time familiarizing myself with the existing codebase (particularly, the DTOs, database setup, and schema) and the requirements of the project.
+- I integrated the .NET application with a starter .NET + Angular template to get the project up and running in Visual Studio.
---
+++ PlanetaryExplorationLogs.API/Migrations/20241108201238_NameOfMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetaryExplorationLogs.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameOfMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
