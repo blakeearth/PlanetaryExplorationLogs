18cc5b6dd6dcb32ecee202b571324d49533ced5d
Isaac Blake <blakeearth@users.noreply.github.com>
2024-11-11 17:40:37 -0600

    Update interceptor to functional interceptor for Angular update

--- planetaryexplorationlogs.client/src/main.ts
+++ planetaryexplorationlogs.client/src/main.ts
@@ -1,11 +1,9 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { environment } from './environments/environment';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { RetryInterceptor } from './app/core/interceptors/retry.interceptor';
+import { retryInterceptor } from './app/core/interceptors/retry.interceptor';
 
 if (environment.production) {
   enableProdMode();
@@ -13,6 +11,8 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: RetryInterceptor, multi: true }
+    provideHttpClient(
+      withInterceptors([retryInterceptor])
+    )
   ]
-});
+}).catch(err => console.error(err));
