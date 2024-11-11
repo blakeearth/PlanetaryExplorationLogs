c29db20d42692c66feb35380fb93eb0d81f21989
Isaac Blake <blakeearth@users.noreply.github.com>
2024-11-10 18:37:56 -0600

    Add ability to GET a planet

---
+++ PlanetaryExplorationLogs.API/Requests/Queries/Planets/GetPlanet/GetPlanet_Handler.cs
@@ -0,0 +1,39 @@
+﻿using PlanetaryExplorationLogs.API.Data.DTO;
+using Microsoft.EntityFrameworkCore;
+using PlanetaryExplorationLogs.API.Utility.Patterns;
+using PlanetaryExplorationLogs.API.Data.Context;
+using static PlanetaryExplorationLogs.API.Utility.Patterns.CommandQuery;
+
+namespace PlanetaryExplorationLogs.API.Requests.Queries.Planets.GetPlanet
+{
+    public class GetPlanet_Handler : HandlerBase<PlanetFormDto>
+    {
+        private readonly int _planetId;
+
+        public GetPlanet_Handler(PlanetExplorationDbContext context, int planetId)
+            : base(context)
+        {
+            _planetId = planetId;
+        }
+
+        public override async Task<RequestResult<PlanetFormDto>> HandleAsync()
+        {
+            var planet = await DbContext.Planets
+                .Where(p => p.Id == _planetId)
+                .Select(p => new PlanetFormDto
+                {
+                    Name = p.Name,
+                    Climate = p.Climate,
+                    Terrain = p.Terrain,
+                    Population = p.Population,
+                    Type = p.Type
+
+                }).FirstOrDefaultAsync();
+
+            var result = new RequestResult<PlanetFormDto> { Data = planet };
+
+            return result;
+        }
+    }
+
+}
---
+++ PlanetaryExplorationLogs.API/Requests/Queries/Planets/GetPlanet/GetPlanet_Query.cs
@@ -0,0 +1,22 @@
+﻿using PlanetaryExplorationLogs.API.Data.DTO;
+using PlanetaryExplorationLogs.API.Data.Context;
+using static PlanetaryExplorationLogs.API.Utility.Patterns.CommandQuery;
+using PlanetaryExplorationLogs.API.Requests.Queries.Planets.GetPlanet;
+
+namespace PlanetaryExplorationLogs.API.Requests.Queries.Planets.GetPlanetsDropdownList
+{
+    public class GetPlanet_Query : RequestBase<PlanetFormDto>
+    {
+        public GetPlanet_Query(PlanetExplorationDbContext context, int planetId)
+            : base(context)
+        {
+            _planetId = planetId;
+        }
+
+        int _planetId;
+
+        public override IValidator Validator => new GetPlanet_Validator(DbContext);
+        public override IHandler<PlanetFormDto> Handler => new GetPlanet_Handler(DbContext, _planetId);
+    }
+
+}
